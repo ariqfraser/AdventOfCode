0a0d3468f74935bfc71d288541ff79b283f8894b
ariq <ariqfraser@gmail.com>
2024-12-02 15:24:44 +0000

    2023 day 3 part 1

---
+++ 2023/day3/part1.py
@@ -0,0 +1,45 @@
+import re
+file = open('part1.txt', 'r')
+
+def hasSpecialChars(charArr):
+    for c in charArr:
+        if re.match('[^\\d.]', c) is not None:
+            return True
+    return False
+
+'''
+NOTE THIS IS PRETTY JANK BUT I CBA TO REFACTOR
+'''
+
+charMatrix = [list(row.replace('\n', '')) for row in file.readlines()]
+rowLength = len(charMatrix[0])
+matrixSize = len(charMatrix)
+schematic = []
+for rowI, row in enumerate(charMatrix):
+    startI = None
+    endI = None
+    for charI, char in enumerate(charMatrix[rowI]):
+        if startI is None and re.match('\d', char):
+            startI = charI - 1 if charI > 0 else 0
+        if startI is not None and endI is None and (re.match('[^\\d]', char) or charI == rowLength - 1):
+            endI = charI + 1
+        
+        if startI != None and endI != None:
+            # print(f'{rowI} check range {startI} - {endI}')
+            hasSpecialAbove, hasSpecialBellow, hasSpecialInline = False, False, hasSpecialChars(charMatrix[rowI][startI:endI])
+            if rowI != 0:
+                hasSpecialAbove = hasSpecialChars(charMatrix[rowI - 1][startI:endI])
+            if rowI < matrixSize - 1:
+                hasSpecialBellow = hasSpecialChars(charMatrix[rowI + 1][startI:endI])
+            # print(f'{rowI} {charMatrix[rowI][startI:endI]} {hasSpecialAbove} {hasSpecialInline} {hasSpecialBellow}')
+
+            if hasSpecialAbove or hasSpecialBellow or hasSpecialInline:
+                schematicStr = ''
+                for c in charMatrix[rowI][startI:endI]:
+                    if re.match('\\d', c):
+                        schematicStr += c
+                schematic.append(int(schematicStr))
+            startI, endI = None, None 
+
+print('Schematic',schematic)
+print('Sum', sum(schematic))
